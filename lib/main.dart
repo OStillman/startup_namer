c13fd90830466bce3a296ce406aa7bd58b3e5835
OStillman <owenstillman@live.co.uk>
2019-04-28 15:54:50 +0100

    Step 3 Change colour

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup name generatorr',
+      theme: ThemeData(
+        primaryColor: Colors.green[700],
+      ),
       home: RandomWords(),
     );
   }
